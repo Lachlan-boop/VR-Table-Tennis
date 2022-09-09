5dcdbd07a0da91161c90a9efa31bb6e3cba8c413
Lachlan-boop <l.f.dean@student.saintkentigern.com>
2022-09-09 12:30:46 +1200

    Version 12.0.0
    Added pick up / drop functionality

--- VRTableTennis/Assets/Scripts/bounce.cs
+++ VRTableTennis/Assets/Scripts/bounce.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class bounce : MonoBehaviour
 {
     public float minVelocity;
     Vector3 lastFrameVelocity;
 
-    
+    public InputActionMap actionMap;
+    public InputAction pickupAction;
+
+    private void Start()
+    {
+        pickupAction.started += ctx => pickupBall();
+        pickupAction.canceled += ctx => DropBall();
+    }
+
+    private void Update()
+    {//Axis1D.PrimaryIndexTrigger   
+
+    }
+
+    private void OnEnable()
+    {
+        pickupAction = actionMap.FindAction("PickupBall");
+        pickupAction.Enable();
+
+    }
+
+    private void pickupBall()
+    {
+        print("PICKUP");
+    }
+
+    private void DropBall()
+    {
+        print("drop");
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         lastFrameVelocity = GetComponent<Rigidbody>().velocity;
