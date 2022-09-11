8b93b13fd1686ce4342343de1daed441616974df
Lachlan-boop <54565878+Lachlan-boop@users.noreply.github.com>
2022-09-11 22:57:54 +1200

    Version 13.0.0
    -Added room
    -Changed GUI
    -Somewhat fixed ball bounce physics

--- VRTableTennis/Assets/Scripts/bounce.cs
+++ VRTableTennis/Assets/Scripts/bounce.cs
@@ -41,9 +41,9 @@ public class bounce : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        lastFrameVelocity = GetComponent<Rigidbody>().velocity;
-        var speed = lastFrameVelocity.magnitude;
-        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collision.contacts[0].normal);
-        GetComponent<Rigidbody>().velocity = direction * Mathf.Max(speed, minVelocity);
+        //lastFrameVelocity = GetComponent<Rigidbody>().velocity;
+        //var speed = lastFrameVelocity.magnitude;
+        //var direction = Vector3.Reflect(lastFrameVelocity.normalized, collision.contacts[0].normal);
+        //GetComponent<Rigidbody>().velocity = direction * Mathf.Max(speed, minVelocity);
     }
 }
